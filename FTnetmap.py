e3c7148b85c70b86ac81cba44045d50e1e70527d
Furkan Tokaç <developmentft@gmail.com>
2015-12-10 16:53:41 +0200

    Update FTnetmap.py
    More modular.

--- FTnetmap.py
+++ FTnetmap.py
@@ -51,11 +51,20 @@ def main():
     verbose  = parser.verbose
     fileName = parser.fileName
     
-    if "-" in ip and len(ip.split(".")) == 7:
-        ipRangeHandler(ip)
-    else:
-        print "[!] ERROR: Please write IPs in correct format."
-        usage()
+    start_scanning(ip)
+    aliveHostsIds.sort()
+    printHosts()
+
+def start_scanning(ip):
+    
+    ipfirst, iplast = handle_ip_format(ip)
+    # Scan info
+    print "[+] Verbose mode : " + ("ON" if verbose else "OFF")
+    print "[+] Export mode  : " + (fileName if fileName!="" else "OFF")
+    print "[+] IP range     : " + ip.split("-")[0]
+    print "                   " + ip.split("-")[1]
+    print "[.] Scanning..."
+    ping_to_range(ipfirst,iplast)
     
     #Sleep till all threads finish
     repeatCounter=0
@@ -67,32 +76,30 @@ def main():
         else:
             repeatCounter+=1
         time.sleep(1)
-    
-    aliveHostsIds.sort()
-    printHosts()
-    
+
 def printHosts():
     global fileName
     
-    print "\nAlive Hosts"
-    if fileName is not "":
+    print "\n[+] Alive Hosts"
+    if fileName!="":
         fileName = open(fileName, "w")
         for i in aliveHostsIds:
-            print "[+]", aliveHosts[i]
+            print aliveHosts[i]
             fileName.write(aliveHosts[i]+"\n")
         fileName.close()
     else:
         for i in aliveHostsIds:
-            print "[+]", aliveHosts[i]
+            print aliveHosts[i]
     print len(aliveHostsIds), "hosts are alive.\n"
 
-def ipRangeHandler(ip):
-    global threadNumber
+def handle_ip_format(ip):
+    if "-" not in ip or len(ip.split(".")) != 7:
+        print "[!] ERROR: Please write IPs in correct format."
+        usage()
     
     ipfirst = (ip.split("-")[0]).split(".") # store ip like ("192", "168", "1", "0")
     iplast  = (ip.split("-")[1]).split(".") # store ip like ("192", "168", "2", "0")
     
-    # Format checking. If format is ok, it will continue.
     for i in xrange(3):
         if iplast[i]=="":
             iplast[i]="0"
@@ -107,28 +114,22 @@ def ipRangeHandler(ip):
         elif int(ipfirst[i])<0:
             ipfirst[i]="0"
 
-    ipfirstint = convertIpInt(map(str, ipfirst))
-    iplastint  = convertIpInt(map(str, iplast))
+    ipfirstint = convert_ip_int(map(str, ipfirst))
+    iplastint  = convert_ip_int(map(str, iplast))
         
     if ipfirstint>=iplastint:
         print "[!] ERROR: First IP cannot be bigger or equal to last IP."
         usage()
-        
-    # String to integer
-    ipfirst = map(int, ipfirst)
-    iplast = map(int, iplast)
     
-    # Scan info
-    print "[+] IP range     : " + ip.split("-")[0]
-    print "                   " + ip.split("-")[1]
-    print "[+] Verbose mode : " + ("ON" if verbose else "OFF")
-    print "[+] Export mode  : " + (fileName if fileName!="" else "OFF")
-    print "[.] Scanning...\n"
+    return map(int, ipfirst), map(int, iplast)
+
+def ping_to_range(ipfirst, iplast):
+    global threadNumber
     
     hostId = 0
     # Start main process
-    ipfirstint = convertIpInt(map(str, ipfirst))
-    iplastint  = convertIpInt(map(str, iplast))
+    ipfirstint = convert_ip_int(map(str, ipfirst))
+    iplastint  = convert_ip_int(map(str, iplast))
     while iplastint > ipfirstint:
         currentIP = str(ipfirst[0]) + "." + str(ipfirst[1]) + "." + str(ipfirst[2]) + "." + str(ipfirst[3])
         threadNumber+=1
@@ -138,8 +139,8 @@ def ipRangeHandler(ip):
         ipThread.start()
         hostId+=1
         
-        ipfirstint = convertIpInt(map(str, ipfirst))
-        iplastint  = convertIpInt(map(str, iplast))
+        ipfirstint = convert_ip_int(map(str, ipfirst))
+        iplastint  = convert_ip_int(map(str, iplast))
         
         # This part makes accurate increment in ip
         ipfirst[3] += 1
@@ -156,7 +157,7 @@ def ipRangeHandler(ip):
                     ipfirst[1] = 0
                     ipfirst[0] += 1
 
-def convertIpInt(ip):
+def convert_ip_int(ip):
     return int(ip[0]+((3-len(ip[1]))*'0'+ip[1])+((3-len(ip[2]))*'0'+ip[2])+((3-len(ip[3]))*'0'+ip[3]))
 
 # if ping successful, return 1 else return 0
@@ -166,9 +167,9 @@ def pingIp(ip, hostId):
     global threadNumber
     
     if PLATFORM == 'Linux':
-        output = runCommand("ping -c 1 -w 1 "+ip)
+        output = runCommand("ping -c 1 -w 1.5 "+ip)
     elif PLATFORM == 'Windows':
-        output = runCommand("ping -n 1 -w 1000 "+ip)
+        output = runCommand("ping -n 1 -w 1500 "+ip)
     
     if  output == False or "Unreachable" in output or "timed out" in output:
         threadNumber-=1
@@ -176,7 +177,7 @@ def pingIp(ip, hostId):
     else:
         if verbose:
             print "%s %8s" % (ip, "ALIVE")
-            
+        
         aliveHosts[hostId] = ip # This keeps IP's in dictionary
         aliveHostsIds.append(hostId) # This keeps just ID's which we assigned. This necessary to sort IP number for output
         threadNumber-=1 # One of the threads finished.
