c9f4c36136b53fe7fdfb1e1419739cb5cec84f83
Furkan Tokaç <developmentft@gmail.com>
2015-12-10 16:58:01 +0200

    Update FTnetmap.py

--- FTnetmap.py
+++ FTnetmap.py
@@ -1,5 +1,17 @@
-# Example   : python FTnetmap.py -r 192.168.0.0-192.168.2.0
-# if you run like python FTnetmap.py, defaultly assing ip 192.168.0.0-192.168.2.0
+'''Example   : python FTnetmap.py -r 192.168.0.0-192.168.2.0
+   if you run like python FTnetmap.py, defaultly assing ip 192.168.0.0-192.168.2.0
+Functions
+    usage()
+    parse_arguments()
+    main()
+    start_scanning(ip)
+    printHosts()
+    handle_ip_format(ip)
+    ping_to_range(ipfirst, iplast)
+    convert_ip_int(ip)
+    pingIp(ip, hostId)
+    runCommand(command)
+'''
 
 import subprocess, sys, argparse, threading, time, platform, socket
 
